c2668ebfd3badbc5be98bcde61d9a7b2e2a61d48
wei <sanirvana8105@gmail.com>
2020-01-16 00:47:59 +0800

    for loop init

--- app.js
+++ app.js
@@ -30,23 +30,23 @@ async function start() {
     /*
      * Start
      */
-    await driver.get('https://www.nul.nagoya-u.ac.jp/db/DBannai/ezproxy/index.html');
-    await driver.findElement(By.linkText("聞蔵(きくぞう)II ビジュアル")).click();
+    // await driver.get('https://www.nul.nagoya-u.ac.jp/db/DBannai/ezproxy/index.html');
+    // await driver.findElement(By.linkText("聞蔵(きくぞう)II ビジュアル")).click();
 
-    await new Promise(resolve => setTimeout(resolve, 10000));
-    await driver.sleep(5000);
+    // await new Promise(resolve => setTimeout(resolve, 10000));
+    // await driver.sleep(5000);
 
-    /*
-    * Login
-    */
-    await driver.wait(until.elementLocated(By.name('username')), 5000);
-    await driver.wait(until.elementLocated(By.name('password')), 5000);
+    // /*
+    // * Login
+    // */
+    // await driver.wait(until.elementLocated(By.name('username')), 5000);
+    // await driver.wait(until.elementLocated(By.name('password')), 5000);
 
-    await driver.findElement(By.name('username')).sendKeys('****');
-    await driver.findElement(By.name('password')).sendKeys('****');
-    await driver.findElement(By.className('btn-submit')).click();
+    // await driver.findElement(By.name('username')).sendKeys('****');
+    // await driver.findElement(By.name('password')).sendKeys('****');
+    // await driver.findElement(By.className('btn-submit')).click();
 
-    await driver.sleep(5000);
+    // await driver.sleep(5000);
 
     await driver.switchTo().frame(1);
 
@@ -54,39 +54,50 @@ async function start() {
     * Search
     */
     // 對象報紙名
-    await driver.findElement(By.xpath("//*[@id='optNotNavi2']/nobr[2]/label")).click();
-    await driver.findElement(By.xpath("//*[@id='optNotNavi2']/nobr[3]/label")).click();
-    await driver.findElement(By.xpath("//*[@id='optNotNavi2']/nobr[4]/label")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi2']/nobr[2]/label")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi2']/nobr[3]/label")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi2']/nobr[4]/label")).click();
 
-    // 關鍵字
-    await driver.findElement(By.id('txtWord_ID')).sendKeys('にくい');
+    // // 關鍵字
+    // await driver.findElement(By.id('txtWord_ID')).sendKeys('にくい');
 
-    // 發行日
-    await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[1]/option[4]")).click();
-    await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[2]/option[2]")).click();
-    await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[3]/option[5]")).click();
+    // // 發行日
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[1]/option[4]")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[2]/option[2]")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[3]/option[5]")).click();
 
-    await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[5]/option[4]")).click();
-    await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[6]/option[13]")).click();
-    await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[7]/option[35]")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[5]/option[4]")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[6]/option[13]")).click();
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi6']/select[7]/option[35]")).click();
 
-    // 順序
-    await driver.findElement(By.id("rdoDspOrder2")).click();
+    // // 順序
+    // await driver.findElement(By.id("rdoDspOrder2")).click();
 
-    // 搜尋
-    await driver.findElement(By.xpath("//*[@id='optNotNavi4']/input[2]")).click();
+    // // 搜尋
+    // await driver.findElement(By.xpath("//*[@id='optNotNavi4']/input[2]")).click();
 
-    await driver.sleep(5000);
+    // await driver.sleep(5000);
 
-    await driver.findElement(By.name("next")).click();
+    for (var i = 4; i < 42; i++) {
 
-    let eles = await driver.findElements(By.xpath("/html/body/center[2]/form/table/tbody/tr[4]/td/table/tbody/tr[1]"));
-    eles.forEach(function (element, index) {
-        element.getText().then(function (text) {
-            console.log('buttonInnerText');
-            console.log(text);
-        }, console.log);
-    });
+        let eles = await driver.findElements(By.xpath("/html/body/center[2]/form/table/tbody/tr[4]/td/table/tbody/tr[" + i + "]"));
+        eles.forEach(function (element, index) {
+            element.getText().then(function (text) {
+                console.log('Row > ' + i);
+                console.log(text);
+            }, console.log);
+        });
+
+        i++;
+
+        eles = await driver.findElements(By.xpath("/html/body/center[2]/form/table/tbody/tr[4]/td/table/tbody/tr[" + i + "]"));
+        eles.forEach(function (element, index) {
+            element.getText().then(function (text) {
+                console.log('Row > ' + i);
+                console.log(text);
+            }, console.log);
+        });
+    }
 
     /**
      * Write Data
